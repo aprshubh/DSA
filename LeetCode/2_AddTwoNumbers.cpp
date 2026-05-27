9d8baa5b87556c3738c8b889d88e9b7fc763e532
Shubham <shubhpatel00007@gmail.com>
2026-05-27 22:43:57 +0530

    AlgoSync: Solved 2. Add Two Numbers (LeetCode - Medium)

---
+++ LeetCode/2_AddTwoNumbers.cpp
@@ -0,0 +1,42 @@
+/**
+ * Problem Link : https://leetcode.com/problems/add-two-numbers/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        int carry = 0;
+        ListNode* curr = dummy;
+        while (l1 || l2 || carry > 0) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            carry = sum / 10;
+        }
+        return dummy->next;
+    }
+};
