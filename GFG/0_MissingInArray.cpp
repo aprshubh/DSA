bb441d888b2cfeaa3da3a7a4e5b030822b7dd435
Shubham <shubhpatel00007@gmail.com>
2026-05-29 04:05:34 +0530

    Sync GFG - Missing in Array (Medium)

---
+++ GFG/0_MissingInArray.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	int missingNum(vector<int>& arr) {
+		long long sum = 0;
+		int n = arr.size();
+		for (int i = 0 ; i<n; i++) {
+			sum += arr[i];
+		}
+		return (1LL*(n + 1)*(n + 2))/2 - sum;
+		
+	}
+};
+
