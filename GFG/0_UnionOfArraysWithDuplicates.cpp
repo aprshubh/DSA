3ff936d5b721d8d580ef213016f304148b767cfb
Shubham <shubhpatel00007@gmail.com>
2026-06-03 01:08:53 +0530

    Sync GFG - Union of Arrays with Duplicates (Medium)

---
+++ GFG/0_UnionOfArraysWithDuplicates.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+
+        unordered_set<int> st(a.begin(), a.end());
+        
+        
+        st.insert(b.begin(), b.end());
+        
+
+        return vector<int>(st.begin(), st.end());
+    }
+};
