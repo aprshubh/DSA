31aca097a93c31bed2911ad3acf5504f7b7074d7
Shubham <shubhpatel00007@gmail.com>
2026-06-02 16:17:13 +0530

    Sync GFG - Array Leaders (Medium)

---
+++ GFG/0_ArrayLeaders.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	vector<int> leaders(vector<int>& arr) {
+		std::vector<int> ans;
+		int n = arr.size() - 1;
+		int largest = arr[n];
+		for (int i = n; i >= 0; i--) {
+			if (arr[i] >= largest)
+				{ans.push_back(arr[i]);
+			largest = arr[i];
+		}
+		
+	}
+	reverse(ans.begin(), ans.end());
+	
+	return ans;
+	
+}
+};
+
