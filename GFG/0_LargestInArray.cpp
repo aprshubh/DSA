309050127af32fc3b2e366adc3b8c512bc2a4cdc
Shubham <shubhpatel00007@gmail.com>
2026-06-03 01:10:22 +0530

    Sync GFG - Largest in Array (Medium)

---
+++ GFG/0_LargestInArray.cpp
@@ -0,0 +1,20 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+       int max = INT_MIN;
+       for (int i = 0;i<arr.size();i++){
+           if ( max<arr[i]) max = arr[i];
+       }return max;
+        
+    }
+};
+
