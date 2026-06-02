8fa575e27a343c2e9b20976337ee67ca95ac09d9
Shubham <shubhpatel00007@gmail.com>
2026-06-03 00:47:49 +0530

    Sync GFG - Array Subset (Medium)

---
+++ GFG/0_ArraySubset.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/array-subset-of-another-array2317/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        
+        std::unordered_map<int,int> mp;
+        for(int x :a){
+            mp[x]++;
+        }
+        for(int x :b){
+            if(!mp[x]) return false;
+            mp[x]--;
+        }return true;
+        
+        
+        
+    }
+};
