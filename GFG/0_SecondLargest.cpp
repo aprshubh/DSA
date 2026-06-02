7a90b011b853d8fc147fabcf6a012b97907b44d9
Shubham <shubhpatel00007@gmail.com>
2026-06-02 14:45:08 +0530

    Sync GFG - Second Largest (Medium)

---
+++ GFG/0_SecondLargest.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/second-largest3735/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getSecondLargest(vector<int> &arr) {
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if (num < largest && num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+
+        return secondLargest;
+    }
+};
+
