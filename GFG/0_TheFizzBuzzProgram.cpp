2c195956dc5daa3e978022d9280688ae18449eba
Shubham <shubhpatel00007@gmail.com>
2026-06-04 02:03:58 +0530

    Sync GFG - The FizzBuzz Program (Medium)

---
+++ GFG/0_TheFizzBuzzProgram.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/the-fizzbuzz-program/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	void fizzBuzz(int number) {
+		if (number%15 == 0)
+			cout << "FizzBuzz"<<endl;
+		else if (number%3 == 0)
+			cout << "Fizz"<<endl;
+		else if (number%5 == 0)
+			cout << "Buzz"<<endl;
+		else
+			cout << number<<endl;
+	}
+};
+
