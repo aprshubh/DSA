4cab3222a871317225fdbe0128552745455b522f
Shubham <shubhpatel00007@gmail.com>
2026-06-04 01:40:08 +0530

    Sync GFG - Sum of Natural Numbers (Medium)

---
+++ GFG/0_SumOfNaturalNumbers.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/reverse-coding2452/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        return (n*(n+1))/2;
+    }
+};
