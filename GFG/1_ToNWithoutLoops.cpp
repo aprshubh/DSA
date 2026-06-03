638000b768ec8b9e1e8a10a8036db5180bbb3fba
Shubham <shubhpatel00007@gmail.com>
2026-06-04 01:50:45 +0530

    Sync GFG - to n Without Loops (Medium)

---
+++ GFG/1_ToNWithoutLoops.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops3621/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	void printTillN(int n) {
+		if (n == 1) {
+			cout << n << " ";
+			return;
+		}
+		
+		printTillN(n - 1);
+		cout << n << " ";
+	}
+};
+
