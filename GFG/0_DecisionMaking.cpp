c16a88d32ef82a42e2ee04cebba114c6089f7f18
Shubham <shubhpatel00007@gmail.com>
2026-06-04 02:16:02 +0530

    Sync GFG - Decision Making (Medium)

---
+++ GFG/0_DecisionMaking.cpp
@@ -0,0 +1,26 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/decision-making/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin>> n>> m;
+	
+	// code here
+	if (n<m)
+		cout << "less";
+	else if (n>m)
+		cout << "greater";
+	else cout << "equal";
+	
+	return 0;
+}
+
