f4bcf244f85562f5b04a2b00a138b668a1bbac37
Shubham <shubhpatel00007@gmail.com>
2026-06-04 01:52:02 +0530

    Sync GFG - Swap The Numbers (Medium)

---
+++ GFG/0_SwapTheNumbers.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/swap-the-numbers/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+	
+}
+
