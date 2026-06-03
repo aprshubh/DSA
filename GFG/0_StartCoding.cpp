8256aef157f901e529e105c5ce9c42d2a7d7d20d
Shubham <shubhpatel00007@gmail.com>
2026-06-04 01:55:53 +0530

    Sync GFG - Start Coding (Medium)

---
+++ GFG/0_StartCoding.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/start-coding-1/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    cout<<"Hello World";
+
+    return 0;
+}
