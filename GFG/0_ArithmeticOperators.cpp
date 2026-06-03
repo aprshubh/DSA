f4e68c94ab3a129f17a77d3700a5829ee47f6ea4
Shubham <shubhpatel00007@gmail.com>
2026-06-04 02:20:22 +0530

    Sync GFG - Arithmetic Operators (Medium)

---
+++ GFG/0_ArithmeticOperators.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/arithmetic-operators-1605594922/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin>> x>> y;
+	
+	// codp = x + y, Addition
+	int p = x + y;
+	int q = x - y;
+	int r = x * y;
+	int s = x / y;
+	int t = x % y;
+	
+	// The below code prints the output
+	cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+	return 0;
+}
+
