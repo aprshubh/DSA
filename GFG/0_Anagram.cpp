1588b2e69598de1d1ee35a716f0ad1e56ae60b61
Shubham <shubhpatel00007@gmail.com>
2026-06-03 16:46:55 +0530

    Sync GFG - Anagram (Medium)

---
+++ GFG/0_Anagram.cpp
@@ -0,0 +1,35 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/anagram-1587115620/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        
+        // if ( s1.size()!=s2.size()) return false;
+        
+        
+        // std::unordered_map<char,int> mp;
+        // for(int x :s1){
+        //     mp[x]++;
+        // }
+        // for(int x :s2 ){
+        //     if(!mp[x]) return false;
+        //     mp[x]--;
+        // }
+        // return true;
+        if ( s1.size()!=s2.size()) return false;
+        
+        int arr[26]={};
+        for (char x : s1) arr[x -'a']++;
+        for (char x : s2) {
+            if(!arr[x-'a']) return false;
+            arr[x -'a']--;
+        }return true;
+    }
+};
