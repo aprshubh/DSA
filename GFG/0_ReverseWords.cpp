48ee864020f50a9df2a63e1f57e5d15b9e5aec5c
Shubham <shubhpatel00007@gmail.com>
2026-06-03 17:17:27 +0530

    Sync GFG - Reverse Words (Medium)

---
+++ GFG/0_ReverseWords.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	string reverseWords(string &s) {
+		// code here
+		vector<string> words;
+		stringstream ss(s);
+		string token;
+		while (getline(ss, token, '.')) {
+			if (token.size())
+				words.push_back(token);
+		} reverse(words.begin(), words.end());
+		string ans = "";
+		for (int i = 0; i<words.size(); i++) {
+			
+			ans += words[i];
+			if (i != words.size() - 1)
+				ans += ".";
+		} return ans;
+	}
+};
+
