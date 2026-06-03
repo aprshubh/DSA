ede99c5361d7f7f7355833069a33fbd6746fadd1
Shubham <shubhpatel00007@gmail.com>
2026-06-03 16:01:47 +0530

    Sync GFG - Missing And Repeating (Medium)

---
+++ GFG/0_MissingAndRepeating.cpp
@@ -0,0 +1,32 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	vector<int> findTwoElement(vector<int>& arr) {
+		
+		int i = 0;
+		
+		int n = arr.size();
+		
+		while (i<n) {
+			int correct = arr[i]-1;
+			if (arr[i] != arr[correct])
+				swap(arr[i], arr[correct]);
+			else
+				i++;
+		}
+		for (int k = 0 ; k<n; k++) {
+			if (arr[k] != k + 1)
+				return {arr[k], k + 1};
+		} return {};
+		
+	}
+};
+
