43e63966a848f53e90a061af223847096f4ec32f
Shubham <shubhpatel00007@gmail.com>
2026-06-04 02:27:06 +0530

    Sync GFG - Array Traversal (Medium)

---
+++ GFG/0_ArrayTraversal.cpp
@@ -0,0 +1,13 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/array-traversal/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void arrayTraversal(int numbers[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << numbers[i] << " ";
+}
