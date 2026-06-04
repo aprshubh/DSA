9398969f08cc8c356b5a455a7b0abf12f0481b7c
Shubham <shubhpatel00007@gmail.com>
2026-06-04 13:38:15 +0530

    Sync GFG - Binary Search (Medium)

---
+++ GFG/0_BinarySearch.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/who-will-win-1587115621/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	bool binarySearch(vector<int>& arr, int k) {
+		int left = 0;
+		int right = arr.size() - 1;
+		while (left<=right) {
+			int mid = left + ((right - left)>> 1);
+			if (k == arr[mid])
+				return true;
+			else if (k>arr[mid])
+				left = mid + 1;
+			else
+				right = mid - 1;
+		} return false;
+		
+	}
+};
+
