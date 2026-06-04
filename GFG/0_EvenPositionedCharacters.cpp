0fcaeee4f6e7ed35d037948346fbad13ad8c5153
Shubham <shubhpatel00007@gmail.com>
2026-06-04 12:43:22 +0530

    Sync GFG - Even Positioned Characters (Medium)

---
+++ GFG/0_EvenPositionedCharacters.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/for-loop-2/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void utility(string s) {
+    // length of string is given by s.length()
+    // character at any index i is given by s[i]
+
+    // Write your code here
+    string ans="";
+    for (int i = 0;i<s.length();i+=2){
+        ans+=s[i];
+    }cout <<ans;
+}
