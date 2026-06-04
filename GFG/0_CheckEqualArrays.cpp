cb145585511d4763ab2c990261377e907e8cacd7
Shubham <shubhpatel00007@gmail.com>
2026-06-04 14:56:36 +0530

    Sync GFG - Check Equal Arrays (Medium)

---
+++ GFG/0_CheckEqualArrays.cpp
@@ -0,0 +1,21 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/check-if-two-arrays-are-equal-or-not3847/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        
+        unordered_map<int,int> mp;
+        for (int x :a) mp[x]++;
+        for(int x : b){
+            if(!mp[x]) return false;
+            mp[x]--;
+        }return true;
+    }
+};
