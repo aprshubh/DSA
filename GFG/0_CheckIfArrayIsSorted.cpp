8abfe5099ada7f914388eabc7756a0a1e48311a0
Shubham <shubhpatel00007@gmail.com>
2026-06-04 12:46:20 +0530

    Sync GFG - Check if array is sorted (Medium)

---
+++ GFG/0_CheckIfArrayIsSorted.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        // code here
+        for(int i = 0 ; i<arr.size()-1;i++){
+            if (arr[i]>arr[i+1]) return false;
+        }return true;
+    }
+};
