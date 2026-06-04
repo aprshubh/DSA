43e81618a44fbb373b9bc13eb81986ef77eda902
Shubham <shubhpatel00007@gmail.com>
2026-06-04 15:06:19 +0530

    Sync GFG - Linked List End Insertion (Medium)

---
+++ GFG/0_LinkedListEndInsertion.cpp
@@ -0,0 +1,34 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/linked-list-insertion-1587115620/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+	public:
+	int data;
+	Node* next;
+	Node(int data) {
+		this->data = data;
+		this->next = nullptr;
+	}
+};
+*/
+class Solution {
+	public:
+	Node *insertAtEnd(Node *head, int x) {
+		
+		Node* newNode = new Node(x);
+		
+		if (head==nullptr) return newNode;
+		Node*current = head;
+		while(current->next!=nullptr) current = current->next;
+		current->next=newNode;
+		return head;
+	}
+};
+
