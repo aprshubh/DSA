0ab09547591d4d558cae181d0deae4f71358fa92
Shubham <shubhpatel00007@gmail.com>
2026-06-05 14:47:07 +0530

    Sync GFG - Array End Insert (Basic)

---
+++ GFG/Basic/0_ArrayEndInsert.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/array-insert-at-end/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void insertAtEnd(vector<int> &arr, int val) {
+        // code here
+        arr.push_back(val);
+        
+    }
+};
+
