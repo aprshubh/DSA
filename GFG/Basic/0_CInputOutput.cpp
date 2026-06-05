925ca16639f500bb89b973bddaa4562b98587e2c
Shubham <shubhpatel00007@gmail.com>
2026-06-05 14:49:50 +0530

    Sync GFG - C++ Input / Output (Basic)

---
+++ GFG/Basic/0_CInputOutput.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/c-input-output2432/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int multiplication(int A, int B) {
+        return A*B;
+        // code here
+        
+    }
+};
