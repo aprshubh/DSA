a8e0dc1dc6e99c8a97e96ee3b87e66f77483c567
Shubham <shubhpatel00007@gmail.com>
2026-06-05 14:45:34 +0530

    Sync GFG - While Loop (Basic)

---
+++ GFG/Basic/0_WhileLoop.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/print-table0303/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void utility(int x) {
+        // code here
+        while(x>=0){
+            cout<<x<<" ";
+            x--;
+        }
+    }
+};
