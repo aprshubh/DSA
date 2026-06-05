fae3e500889d1a2907f1c80e3c8dfb9cb1d9cabf
Shubham <shubhpatel00007@gmail.com>
2026-06-05 14:44:04 +0530

    Sync GFG - Odd or Even (Basic)

---
+++ GFG/Basic/0_OddOrEven.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/odd-or-even3618/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+	bool isEven(int n) {
+		return (n & 1) == 0;
+		
+	}
+};
+
