6bae5a975a3927f78b26b66bc32d610bcf5f2ef3
Shubham <shubhpatel00007@gmail.com>
2026-06-05 18:05:59 +0530

    Sync GFG - Multiply Array (Basic)

---
+++ GFG/Basic/0_MultiplyArray.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/multiply-array-1658312632/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int product(int arr[], int n) {
+
+        int ans = 1;
+
+        for(int i = 0; i < n; i++) {
+            ans *= arr[i];
+        }
+
+        return ans;
+    }
+};
