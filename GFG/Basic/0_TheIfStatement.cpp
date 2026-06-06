c44a802224990ee0c8fd4907b38698c39ca81289
Shubham <shubhpatel00007@gmail.com>
2026-06-06 16:08:18 +0530

    Sync GFG - The If Statement (Basic)

---
+++ GFG/Basic/0_TheIfStatement.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/the-if-statement--113256/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n > 100)
+        cout << "Big" << endl << "Number";
+    else
+        cout << "Number";
+
+    return 0;
+}
