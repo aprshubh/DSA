de490cf3171ec221055ae79e7ad82248da0ad6cc
Shubham <shubhpatel00007@gmail.com>
2026-06-06 16:16:22 +0530

    Sync GFG - Print With Space (Basic)

---
+++ GFG/Basic/0_PrintWithSpace.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/print-with-space/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+void utility() {
+    string a, b;
+    cin >> a >> b;
+    cout<<a<<" "<<b<<endl;
+    // Write your code below that prints a <space> b
+}
