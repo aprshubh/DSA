abd3ccd28e8920f58b02bb5bc720de4184652817
Shubham <shubhpatel00007@gmail.com>
2026-06-07 00:50:54 +0530

    Sync GFG - Sum of AP series (Basic)

---
+++ GFG/Basic/0_SumOfAPSeries.cpp
@@ -0,0 +1,15 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/sum-of-ap-series4512/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int sumOfAP(int n, int a, int d) {
+        return (n * (2*a + (n-1)*d)) / 2;
+    }
+};
