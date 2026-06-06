b1c7fa1292e7a6a357da5cb5787851e4569f3ea2
Shubham <shubhpatel00007@gmail.com>
2026-06-06 16:09:48 +0530

    Sync GFG - Function With Return (Basic)

---
+++ GFG/Basic/0_FunctionWithReturn.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/function-with-return-value/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int returnValueFunction(int n) {
+        // code here
+        return n*2;
+    }
+};
