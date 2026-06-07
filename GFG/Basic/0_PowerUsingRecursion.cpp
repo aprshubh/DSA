16e7bd39e02a767ad0377ebb196998ddb0d152cb
Shubham <shubhpatel00007@gmail.com>
2026-06-07 23:10:50 +0530

    Sync GFG - Power Using Recursion (Basic)

---
+++ GFG/Basic/0_PowerUsingRecursion.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/power-using-recursion/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int RecursivePower(int n, int p) {
+        if (p == 0) return 1;
+
+        return n * RecursivePower(n, p - 1);
+    }
+};
