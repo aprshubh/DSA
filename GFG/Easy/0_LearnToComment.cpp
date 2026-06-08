9b695649fcfedd6173a31af1b12f68f902e48393
Shubham <shubhpatel00007@gmail.com>
2026-06-08 19:12:55 +0530

    Sync GFG - Learn to Comment (Easy)

---
+++ GFG/Easy/0_LearnToComment.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/learn-to-comment/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+
+    // Prints a
+    cout << a << endl;
+
+    // Prints b
+    // cout << b << endl;
+
+    // Prints c
+    cout << c << endl;
+}
