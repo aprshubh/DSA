ff0bb4c7105afbb73887facdd81d09ab3ff759d0
Shubham <shubhpatel00007@gmail.com>
2026-06-09 16:42:25 +0530

    Sync GFG - Print Linked List (Basic)

---
+++ GFG/Basic/0_PrintLinkedList.cpp
@@ -0,0 +1,39 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/print-linked-list-elements/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> printList(Node *head) {
+        // code here
+        vector<int> arr;
+        while (head){
+            arr.push_back(head->data);
+            head=head->next;
+        }return arr;
+    }
+};
