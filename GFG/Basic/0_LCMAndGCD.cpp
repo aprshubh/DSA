ae32630267cc0844842ce68984e57806f591fbe2
Shubham <shubhpatel00007@gmail.com>
2026-06-10 14:50:26 +0530

    Sync GFG - LCM And GCD (Basic)

---
+++ GFG/Basic/0_LCMAndGCD.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/lcm-and-gcd4516/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> lcmAndGcd(int a, int b) {
+
+        int x = a, y = b;
+
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        int gcd = a;
+        int lcm = (x / gcd) * y;
+
+        return {lcm, gcd};
+    }
+};
