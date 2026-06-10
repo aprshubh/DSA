eb9bb578f6a58f9c6bf8eebdfa9f9aef55d06ca6
Shubham <shubhpatel00007@gmail.com>
2026-06-10 18:16:14 +0530

    Sync GFG - Sort Array II (Easy)

---
+++ GFG/Easy/0_SortArrayII.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/sort-the-array0055/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void sortArr(vector<int> &arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+    }
+};
