d88f7449ef410159945fc48dffc864271bcd04fc
Shubham <shubhpatel00007@gmail.com>
2026-06-11 02:45:49 +0530

    Sync GFG - Queue Push & Pop (Basic)

--- GFG/Basic/0_QueuePushPop.cpp
+++ GFG/Basic/0_QueuePushPop.cpp
@@ -1,5 +1,5 @@
 /**
- * Problem Link : https://practice.geeksforgeeks.org/problems/queue-designer--104629/1
+ * Problem Link : https://practice.geeksforgeeks.org/problems/queue-designer/1
  * Platform     : GFG
  * Difficulty   : Basic
  */
@@ -7,24 +7,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// User function Template for C++
-
-// User function Template for C++
-
-queue<int> _push(vector<int> &arr) {
-    // return a queue with all elements of arr inserted in it
-    queue<int> q;
-    
-    for(int x : arr) 
-        q.push(x);
-        
-    return q;
-}
-
-void _pop(queue<int> &q) {
-    // print front and dequeue for each element until it becomes empty
-    while(!q.empty()){
-        cout<<q.front()<<" ";
-        q.pop();
+class Solution {
+  public:
+    queue<int> fillQ(const vector<int>& arr) {
+        // code here
+        queue<int> q;
+        for(int x : arr) q.push(x);
+        return q;
     }
-}
+
+    void emptyQ(queue<int>& q) {
+        
+        // code here
+        while(!q.empty()){
+            cout<<q.front()<<" ";
+            q.pop();
+            
+        }cout<<endl;
+    }
+};
