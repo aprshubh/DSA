ce8914b6cbeaaed8767319af540d304cabff7c3a
Shubham <shubhpatel00007@gmail.com>
2026-06-12 12:57:22 +0530

    Sync LeetCode - Digit Frequency Score (Easy)

---
+++ LeetCode/Easy/3945_DigitFrequencyScore.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://leetcode.com/problems/digit-frequency-score/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+
+        int sum = 0;
+        while(n>0){
+            int dig = n%10;
+            sum+=dig;
+            n=n/10;
+        }return sum;
+        
+    }
+};
