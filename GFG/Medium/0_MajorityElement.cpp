856b4065dd53e7d716424884733cfab9fed60eea
Shubham <shubhpatel00007@gmail.com>
2026-06-15 00:48:30 +0530

    Sync GFG - Majority Element (Medium)

--- GFG/Medium/0_MajorityElement.cpp
+++ GFG/Medium/0_MajorityElement.cpp
@@ -10,17 +10,44 @@ using namespace std;
 class Solution {
   public:
     vector<int> findMajority(vector<int>& arr) {
-        // Code here
-        if (arr.size()<3) arr;
+        int n = arr.size();
         
-        vector <int> ans;
-        unordered_map<int,int> mp;
+       
+        int cand1 = 0, cand2 = 0;
+        int count1 = 0, count2 = 0;
         
-        int n = arr.size()/3;
-        for(int x : arr) mp[x]++;
-        for(auto[key,value]:mp){
-            if (value>n)ans.push_back(key);
-        }if(ans.size()==2 && ans[1]<ans[0]) swap(ans[0],ans[1]);
+        for (int x : arr) {
+            if (x == cand1) {
+                count1++;
+            } else if (x == cand2) {
+                count2++;
+            } else if (count1 == 0) {
+                cand1 = x;
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = x;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+       
+        count1 = 0;
+        count2 = 0;
+        for (int x : arr) {
+            if (x == cand1) count1++;
+            else if (x == cand2) count2++;
+        }
+        
+        vector<int> ans;
+        if (count1 > n / 3) ans.push_back(cand1);
+        if (count2 > n / 3) ans.push_back(cand2);
+        
+        
+        sort(ans.begin(), ans.end());
+        
         return ans;
     }
 };
