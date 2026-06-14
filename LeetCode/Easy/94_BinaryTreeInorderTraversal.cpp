3da0c5f6877abfda9fcba6d16c5f7b932fc7651f
Shubham <shubhpatel00007@gmail.com>
2026-06-14 15:13:31 +0530

    Sync LeetCode - Binary Tree Inorder Traversal (Easy)

---
+++ LeetCode/Easy/94_BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Problem Link : https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int>& arr) {
+        if (!root)
+            return;
+        
+        helper(root->left, arr);
+        arr.push_back(root->val);
+        helper(root->right, arr);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        helper(root, arr);
+        return arr;
+    }
+};
