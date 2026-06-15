ae6a5f96f0da08cd7663ba07a24372f2edf1e85a
Shubham <shubhpatel00007@gmail.com>
2026-06-15 16:50:13 +0530

    Sync GFG - Number of occurrence (Easy)

--- GFG/Easy/0_NumberOfOccurrence.cpp
+++ GFG/Easy/0_NumberOfOccurrence.cpp
@@ -8,31 +8,39 @@
 using namespace std;
 
 class Solution {
-	public:
-	int countFreq(vector<int>& arr, int target) {
-		int left = 0;
-		int right = arr.size() - 1;
-		while (left <= right) {
-			int mid = left + ((right - left)>> 1);
-			if (arr[mid] == target) {
-				int count = 0;
-				int midd = mid;
-				while (arr[midd] == target) {
-					count++;
-					midd--;
-					
-				}
-				while (arr[mid + 1] == target)
-					{
-					count++;
-					mid++;
-				} return count;
-				
-			} else if (arr[mid]>target) right = mid - 1;
-			else
-				left = mid + 1;
-		} return 0;
-		
-	}
-};
+public:
+    int countFreq(vector<int>& arr, int target) {
+        int lowIdx = getLowerBound(arr, target);
+        int highIdx = getUpperBound(arr, target);
+        
+        return highIdx - lowIdx;
+    }
 
+private:
+   
+    int getLowerBound(vector<int>& arr, int target) {
+        int left = 0, right = arr.size(); 
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] >= target) { 
+                right = mid;
+                } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    int getUpperBound(vector<int>& arr, int target) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] > target) { 
+                right = mid; 
+                } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
