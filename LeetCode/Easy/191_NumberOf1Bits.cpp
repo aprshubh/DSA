dc22b2125f8d57ed36d0ac2c91838e671b31d0d1
Shubham <shubhpatel00007@gmail.com>
2026-06-17 00:20:25 +0530

    Sync LeetCode - Number of 1 Bits (Medium)

---
+++ LeetCode/Easy/191_NumberOf1Bits.cpp
@@ -0,0 +1,20 @@
+/**
+ * Problem Link : https://leetcode.com/problems/number-of-1-bits/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n >> i) & 1)
+                count++;
+        }
+        return count;
+    }
+};
