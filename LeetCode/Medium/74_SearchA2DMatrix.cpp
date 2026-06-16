6e51f6ddb0ac75fa76e8f95bd0bc5aa3a38455c0
Shubham <shubhpatel00007@gmail.com>
2026-06-17 02:51:45 +0530

    Sync LeetCode - 74. Search a 2D Matrix (Medium)

--- LeetCode/Medium/74_SearchA2DMatrix.cpp
+++ LeetCode/Medium/74_SearchA2DMatrix.cpp
@@ -10,10 +10,31 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i = 0;i<matrix.size();i++){
-            for( int j = 0;j<matrix[i].size();j++){
-                if(matrix[i][j]==target) return true;
-            }
-        }return false;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target)
+                return true;
+
+            else if (matrix[row][col] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return false;
     }
 };
+
