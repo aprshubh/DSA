ac4b6a36ba8b87b28b1668295ce12b1d76087b85
Shubham <shubhpatel00007@gmail.com>
2026-06-16 21:40:05 +0530

    Sync LeetCode - 190. Reverse Bits (Easy)

---
+++ LeetCode/Easy/190_ReverseBits.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://leetcode.com/problems/reverse-bits/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans=0;
+        for(int i = 0;i<32;i++){
+            if((n>>i)&1) ans|=1<<(31-i);
+        }return ans;
+        
+    }
+};
