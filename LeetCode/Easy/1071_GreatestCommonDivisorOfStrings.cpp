5768c5f3acbd6424ac7da55315a151eaf5398d27
Shubham <shubhpatel00007@gmail.com>
2026-06-24 00:12:07 +0530

    Sync LeetCode - 1071. Greatest Common Divisor of Strings (Easy)

---
+++ LeetCode/Easy/1071_GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://leetcode.com/problems/greatest-common-divisor-of-strings/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+
+        if (str1 + str2 != str2 + str1)
+            return "";
+        int len = gcd(str1.size(), str2.size());
+        return str1.substr(0, len);
+    }
+};
