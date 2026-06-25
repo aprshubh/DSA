88a3c2c993767581b238751ba51235dbca654346
Shubham <shubhpatel00007@gmail.com>
2026-06-25 17:26:34 +0530

    Sync LeetCode - 338. Counting Bits (Easy)

---
+++ LeetCode/Easy/338_CountingBits.cpp
@@ -0,0 +1,20 @@
+/**
+ * Problem Link : https://leetcode.com/problems/counting-bits/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+};
