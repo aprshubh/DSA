accb803366c5053a5c5e9078edd253ca53393c95
Shubham <shubhpatel00007@gmail.com>
2026-06-25 15:08:46 +0530

    Sync LeetCode - 735. Asteroid Collision (Medium)

---
+++ LeetCode/Medium/735_AsteroidCollision.cpp
@@ -0,0 +1,47 @@
+/**
+ * Problem Link : https://leetcode.com/problems/asteroid-collision/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+
+        for (int a : asteroids) {
+
+            bool alive = true;
+
+            while (alive && !st.empty() && st.top() > 0 && a < 0) {
+
+                if (abs(st.top()) < abs(a)) {
+                    st.pop();              
+                }
+                else if (abs(st.top()) == abs(a)) {
+                    st.pop();              
+                    alive = false;
+                }
+                else {
+                    alive = false;         
+                }
+            }
+
+            if (alive)
+                st.push(a);
+        }
+
+        vector<int> ans(st.size());
+
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
