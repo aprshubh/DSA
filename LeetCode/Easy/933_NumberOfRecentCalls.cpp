c7428323e15a18ea3312b077d5bc16685af5a0a6
Shubham <shubhpatel00007@gmail.com>
2026-06-25 14:13:47 +0530

    Sync LeetCode - 933. Number of Recent Calls (Easy)

---
+++ LeetCode/Easy/933_NumberOfRecentCalls.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Link : https://leetcode.com/problems/number-of-recent-calls/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class RecentCounter {
+public:
+    queue<int> q;
+
+    RecentCounter() {
+    }
+
+    int ping(int t) {
+        q.push(t);
+
+        while(q.front() < t - 3000)
+            q.pop();
+
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
