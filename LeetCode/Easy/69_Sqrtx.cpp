e1058a5822e94b14069027252955f1ee501ac188
Shubham <shubhpatel00007@gmail.com>
2026-06-26 20:13:10 +0530

    Sync LeetCode - 69. Sqrt(x) (Easy)

--- LeetCode/Easy/69_Sqrtx.cpp
+++ LeetCode/Easy/69_Sqrtx.cpp
@@ -11,21 +11,20 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        int left = 1, right = x, mid;
-        if (x == 0)
-            return 0;
-        if (x < 3)
-            return 1;
-        while (left <= right) {
-            mid = left + ((right - left) >> 1);
+        if (x <= 1)
+            return x;
 
-            if (mid == (x / mid))
-                return mid;
-            else if ((mid) < (x / mid))
+        int left = 1, right = x;
+
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+
+            if (mid <= x / mid)
                 left = mid + 1;
             else
                 right = mid - 1;
         }
+
         return right;
     }
 };
