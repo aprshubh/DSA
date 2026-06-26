4b48705007ef248bc05970926d68dd94e959eab1
Shubham <shubhpatel00007@gmail.com>
2026-06-27 00:29:36 +0530

    Sync LeetCode - 1137. N-th Tribonacci Number (Easy)

---
+++ LeetCode/Easy/1137_NThTribonacciNumber.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://leetcode.com/problems/n-th-tribonacci-number/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n <= 1) return n;
+        if (n == 2) return 1;
+
+        int a = 0, b = 1, c = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+
+        return c;
+    }
+};
