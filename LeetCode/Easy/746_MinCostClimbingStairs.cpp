cbcb0fe9c062678fc89f564216feeb08b7b1c809
Shubham <shubhpatel00007@gmail.com>
2026-06-27 01:25:24 +0530

    Sync LeetCode - 746. Min Cost Climbing Stairs (Easy)

--- LeetCode/Easy/746_MinCostClimbingStairs.cpp
+++ LeetCode/Easy/746_MinCostClimbingStairs.cpp
@@ -9,15 +9,17 @@ using namespace std;
 
 class Solution {
 public:
-   int minCost(vector<int> & cost,int n,vector<int>&dp){
-    if(n==1||n==0) return cost[n];
-    if(dp[n]!=-1) return dp[n];
-    return dp[n]=cost[n]+min(minCost(cost,n-1,dp),minCost(cost,n-2,dp));
-   }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size()-1;
-        vector<int> dp(n+1,-1);
-        return min(minCost(cost,n,dp),minCost(cost,n-1,dp));
-        
+        int n = cost.size();
+
+        int a = cost[0], b = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            int c = cost[i] + min(a, b);
+            a = b;
+            b = c;
+        }
+
+        return min(a, b);
     }
 };
