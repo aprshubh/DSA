45f4aa342f423cf05aa80caffee0b51842521e53
Shubham <shubhpatel00007@gmail.com>
2026-06-27 18:46:47 +0530

    Sync LeetCode - 62. Unique Paths (Medium)

--- LeetCode/Medium/62_UniquePaths.cpp
+++ LeetCode/Medium/62_UniquePaths.cpp
@@ -10,20 +10,19 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        for(int i = 0;i<n;i++){
-            dp[0][i]=1;
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
         }
-        for(int i = 0;i<m;i++){
-            dp[i][0]=1;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
-        for(int i = 1;i<m;i++){
-            for(int k = 1;k<n;k++){
-                dp[i][k]=dp[i-1][k]+dp[i][k-1];
+        for (int i = 1; i < m; i++) {
+            for (int k = 1; k < n; k++) {
+                dp[i][k] = dp[i - 1][k] + dp[i][k - 1];
             }
-        }return dp[m-1][n-1];
-
-
+        }
+        return dp[m - 1][n - 1];
     }
 };
