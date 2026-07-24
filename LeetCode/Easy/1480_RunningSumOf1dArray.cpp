dc413e7f4403242c6b98d7f7d04b5e4ffe277186
Shubham <shubhpatel00007@gmail.com>
2026-07-24 17:17:59 +0530

    Sync LeetCode - 1480. Running Sum of 1d Array (Easy)

---
+++ LeetCode/Easy/1480_RunningSumOf1dArray.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://leetcode.com/problems/running-sum-of-1d-array/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i = 1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }return nums;
+        
+    }
+};
