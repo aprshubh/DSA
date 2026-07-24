93f6983ccd81ab1eac0c9d152684e9cd6c631a79
Shubham <shubhpatel00007@gmail.com>
2026-07-24 14:16:32 +0530

    Sync LeetCode - 414. Third Maximum Number (Easy)

---
+++ LeetCode/Easy/414_ThirdMaximumNumber.cpp
@@ -0,0 +1,35 @@
+/**
+ * Problem Link : https://leetcode.com/problems/third-maximum-number/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+
+        for (int x : nums) {
+            if (x == first || x == second || x == third)
+                continue;
+
+            if (x > first) {
+                third = second;
+                second = first;
+                first = x;
+            } else if (x > second) {
+                third = second;
+                second = x;
+            } else if (x > third) {
+                third = x;
+            }
+        }
+
+        return (third == LLONG_MIN) ? first : third;
+    }
+};
