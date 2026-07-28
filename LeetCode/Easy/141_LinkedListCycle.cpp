64b65bc0e603d83f792ef2329d7e068bd04a4f00
Shubham <shubhpatel00007@gmail.com>
2026-07-28 19:41:25 +0530

    Sync LeetCode - 141. Linked List Cycle (Easy)

--- LeetCode/Easy/141_LinkedListCycle.cpp
+++ LeetCode/Easy/141_LinkedListCycle.cpp
@@ -17,15 +17,17 @@ using namespace std;
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        if(!head) return false;
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast->next && fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast) return true;
-        }return false;
-        
+    bool hasCycle(ListNode* head) {
+        if (!head)
+            return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
     }
 };
