57091efa36086da0727c1de16aff08360c7129d3
Shubham <shubhpatel00007@gmail.com>
2026-07-31 18:08:02 +0530

    Sync LeetCode - 300. Longest Increasing Subsequence (Medium)

---
+++ LeetCode/Medium/300_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Link : https://leetcode.com/problems/longest-increasing-subsequence/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
