8d7338ea9c5933be888da2f87c15f09babb10b25
Shubham <shubhpatel00007@gmail.com>
2026-08-15 16:20:12 +0530

    Sync LeetCode - 704. Binary Search (Easy)

--- LeetCode/Easy/704_BinarySearch.cpp
+++ LeetCode/Easy/704_BinarySearch.cpp
@@ -9,15 +9,21 @@ using namespace std;
 
 class Solution {
 public:
-    int bs(vector<int>& nums, int target , int left,int right){
-        int mid = left +((right-left)>>1);
-        if(nums[mid]==target) return mid;
-        else if(mid>left&& nums[mid]>target) return bs(nums,target,left,mid-1);
-        else if(mid<right && nums[mid]<target) return bs(nums,target ,mid+1,right);
-        else return -1;
+    int bs(vector<int>& nums, int target, int left, int right) {
+        if (left > right) return -1;
+
+        int mid = left + ((right - left) >> 1);
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] > target)
+            return bs(nums, target, left, mid - 1);
+
+        return bs(nums, target, mid + 1, right);
     }
+
     int search(vector<int>& nums, int target) {
-        return bs(nums,target,0,nums.size()-1);
-        
+        return bs(nums, target, 0, nums.size() - 1);
     }
 };
