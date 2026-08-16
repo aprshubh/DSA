ae42e765e631268b6f0c1a3fa2142fd15954edc5
Shubham <shubhpatel00007@gmail.com>
2026-08-17 03:31:04 +0530

    Sync LeetCode - 175. Combine Two Tables (Easy)

---
+++ LeetCode/Easy/175_CombineTwoTables.cpp
@@ -0,0 +1,15 @@
+/**
+ * Problem Link : https://leetcode.com/problems/combine-two-tables/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# Write your MySQL query statement below
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
+
