08d1d07c52ba07e6c8fba7392172138313287240
Shubham <shubhpatel00007@gmail.com>
2026-08-18 01:57:16 +0530

    Sync LeetCode - 1174. Immediate Food Delivery II (Medium)

---
+++ LeetCode/Medium/1174_ImmediateFoodDeliveryII.cpp
@@ -0,0 +1,26 @@
+/**
+ * Problem Link : https://leetcode.com/problems/immediate-food-delivery-ii/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# Write your MySQL query statement below
+SELECT
+    ROUND(
+        AVG(
+            CASE
+                WHEN order_date = customer_pref_delivery_date THEN 1
+                ELSE 0
+            END
+        ) * 100,
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
