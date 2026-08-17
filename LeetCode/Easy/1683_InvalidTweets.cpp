3ceb7517e3b36cbcc6aab85dcc1f380bd7fb2096
Shubham <shubhpatel00007@gmail.com>
2026-08-17 15:16:17 +0530

    Sync LeetCode - 1683. Invalid Tweets (Easy)

---
+++ LeetCode/Easy/1683_InvalidTweets.cpp
@@ -0,0 +1,14 @@
+/**
+ * Problem Link : https://leetcode.com/problems/invalid-tweets/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# Write your MySQL query statement below
+select tweet_id
+from tweets
+where content='More than fifteen chars are here!';
+
