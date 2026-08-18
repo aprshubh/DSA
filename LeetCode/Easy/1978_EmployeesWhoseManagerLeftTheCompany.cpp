b3f5f5fe548055af72fe6b4d5ae91023a8096fca
Shubham <shubhpatel00007@gmail.com>
2026-08-18 14:28:50 +0530

    Sync LeetCode - 1978. Employees Whose Manager Left the Company (Easy)

---
+++ LeetCode/Easy/1978_EmployeesWhoseManagerLeftTheCompany.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://leetcode.com/problems/employees-whose-manager-left-the-company/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+AND manager_id IS NOT NULL
+AND manager_id NOT IN (
+    SELECT employee_id
+    FROM Employees
+);
