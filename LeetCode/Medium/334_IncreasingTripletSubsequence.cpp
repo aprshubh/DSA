d4100765a8fc4a7e57a74c499fe75b8997c0a7a9
Shubham <shubhpatel00007@gmail.com>
2026-08-24 20:08:12 +0530

    Sync LeetCode - 334. Increasing Triplet Subsequence (Medium)

---
+++ LeetCode/Medium/334_IncreasingTripletSubsequence.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://leetcode.com/problems/increasing-triplet-subsequence/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int num : nums) {
+
+            if (num <= first)
+                first = num;
+            else if (num <= second)
+                second = num;
+            else
+                return true;
+        }
+        return false;
+    }
+};
