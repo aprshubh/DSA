23b079a22925f45937781fe1ef5a219f17b715a4
Shubham <shubhpatel00007@gmail.com>
2026-08-26 21:26:32 +0530

    Sync LeetCode - 3731. Find Missing Elements (Easy)

---
+++ LeetCode/Easy/3731_FindMissingElements.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Link : https://leetcode.com/problems/find-missing-elements/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = nums[i - 1] + 1; j < nums[i]; j++) {
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+    }
+};
