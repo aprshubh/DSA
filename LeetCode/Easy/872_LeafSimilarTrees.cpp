aeb62e5ba048435496242c1eeaff2ae80c9c4f93
Shubham <shubhpatel00007@gmail.com>
2026-09-05 22:50:22 +0530

    Sync LeetCode - 872. Leaf-Similar Trees (Easy)

--- LeetCode/Easy/872_LeafSimilarTrees.cpp
+++ LeetCode/Easy/872_LeafSimilarTrees.cpp
@@ -21,36 +21,28 @@ using namespace std;
  */
 class Solution {
 public:
-    void helper1(TreeNode* root1, vector<int>& arr1) {
-        if (!root1)
+    class Solution {
+public:
+    void helper(TreeNode* root, vector<int>& arr) {
+        if (!root) return;
+
+        if (!root->left && !root->right) {
+            arr.push_back(root->val);
             return;
-        if (!root1->left && !root1->right) {
-            arr1.push_back(root1->val);
         }
-        helper1(root1->left, arr1);
-        helper1(root1->right, arr1);
+
+        helper(root->left, arr);
+        helper(root->right, arr);
     }
-    void helper2(TreeNode* root2, vector<int>& arr2) {
-        if (!root2)
-            return;
-        if (!root2->left && !root2->right) {
-            arr2.push_back(root2->val);
-        }
-        helper2(root2->left, arr2);
-        helper2(root2->right, arr2);
-    }
+
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> arr1;
-        vector<int> arr2;
-        helper1(root1, arr1);
-        helper2(root2, arr2);
-        int i = 0;
-        int j = 0;
-        if(arr1.size()!=arr2.size()) return false;
-        while(i<arr1.size() && j<arr2.size()){
-            if(arr1[i]!=arr2[j]) return false;
-            i++;
-            j++;
-        }return true;
+        vector<int> arr1, arr2;
+
+        helper(root1, arr1);
+        helper(root2, arr2);
+
+        return arr1 == arr2;
+    }
+};
     }
 };
