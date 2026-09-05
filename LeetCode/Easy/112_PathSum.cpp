8a1a7a9da6bd20ee1a740fb2d51edb3a42d0b1d8
Shubham <shubhpatel00007@gmail.com>
2026-09-06 01:06:20 +0530

    Sync LeetCode - 112. Path Sum (Easy)

--- LeetCode/Easy/112_PathSum.cpp
+++ LeetCode/Easy/112_PathSum.cpp
@@ -15,15 +15,20 @@ using namespace std;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root) return false;
-        if(!root->left && !root->right) return root->val==targetSum;
-        int rem = targetSum- root->val;
-        return hasPathSum(root->left , rem) || hasPathSum(root->right, rem);
+        if (!root)
+            return false;
+        targetSum -= root->val;
+        if (targetSum == 0 && !root->left && !root->right)
+            return true;
+
+        return hasPathSum(root->left, targetSum) ||
+               hasPathSum(root->right, targetSum);
     }
 };
