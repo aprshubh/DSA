0c2ead9d7bc7f3bdc3fa4208391b05143f05102f
Shubham <shubhpatel00007@gmail.com>
2026-09-06 16:02:08 +0530

    Sync LeetCode - 991. Broken Calculator (Medium)

--- LeetCode/Medium/991_BrokenCalculator.cpp
+++ LeetCode/Medium/991_BrokenCalculator.cpp
@@ -11,19 +11,15 @@ class Solution {
 public:
     int brokenCalc(int startValue, int target) {
         int count = 0;
-        while(target>startValue){
-            if(target%2==0 ){
-                int dig = target/2;
-                if(dig>startValue|| abs(dig-startValue)<abs(dig-target)){
-                    target=target/2;
-                    count++;
-                }
-            }else{
-                target+=1;
-                count++;
-            }
-        }if(target<startValue) count+=abs(target-startValue);
+        while (target != startValue) {
+            if (target < startValue || target % 2 != 0)
+                target++;
+
+            else
+                target /= 2;
+            count++;
+        }
+
         return count;
-        
     }
 };
